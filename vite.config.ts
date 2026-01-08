667fafec8f32b133151879256707220f7edf6a1e
Arthur Rump <hello@arthurrump.com>
2026-01-08 12:24:58 +0100

    Add GitHub action to deploy to pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
   plugins: [devtools(), solidPlugin()],
+  base: process.env.GITHUB_ACTIONS ? '/minicoder/' : '/',
   server: {
     port: 3000,
   },
